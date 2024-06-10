2563b59b5743604cb5114d5db80e180078706a43
jitesh <bhadanedinesh49@gmail.com>
2024-06-10 17:01:51 +0530

    Create user model with it's services

---
+++ src/models/user/services.js
@@ -0,0 +1,19 @@
+const User = require("./index");
+
+exports.getUser = async (data) => {
+    try {
+        const user = await User.findOne(data);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+exports.create = async (userData) => {
+    try {
+        const user = await User.create(userData);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
